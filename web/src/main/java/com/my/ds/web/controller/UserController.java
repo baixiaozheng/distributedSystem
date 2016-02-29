61e2ce2f05ed35b2fee38bebf9676a8bc8917ab8
baixiaozheng <baixiaozheng@com>
2016-02-29 17:57:03 +0800

    添加dubbo的配置

--- web/src/main/java/com/my/ds/web/controller/UserController.java
+++ web/src/main/java/com/my/ds/web/controller/UserController.java
@@ -1,6 +1,41 @@
-package com.my.ds.web.controller; 
+package com.my.ds.web.controller;
 
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.my.ds.model.User;
+import com.my.ds.rpc.domain.UserDomain;
+
+@Controller
+@RequestMapping("/")
 public class UserController {
-	
+    @Resource(name="userDomain")
+	private UserDomain userDomain;
+    
+    @ResponseBody
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public User add(){
+        User user = new User();
+        user.setEmail("email");
+        user.setEnable(true);
+        user.setGithub("github");
+        user.setLastestLoginIp("");
+        user.setLastestLoginTime(null);
+        user.setMobile("18888888888");
+        user.setNickname("nickname");
+        user.setPassword("aaaaaa");
+        user.setQq("qq");
+        user.setRealName("realName");
+        user.setRegTime(new Date());
+        user.setWebsite("webSite");
+        user = userDomain.add(user);
+        return user;
+    }
 }
  
